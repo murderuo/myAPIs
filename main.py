0a3e3804940fb1baea5a633ece1180e762720efc
OkUgur <murderuo@users.noreply.github.com>
2022-06-16 12:39:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,18 +8,6 @@ app = FastAPI()
 infilation=ExploreData()
 infilation_data=infilation.get_data()
 
-origins = [
-    "http://localhost:3000",
-    "localhost:3000"
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
 
 
 
